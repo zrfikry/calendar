e825c5827769c27efa0707c1c838123877f83b2a
Zakky R <zrfikry@gmail.com>
2018-09-22 23:34:30 +0700

    fix week calculation and style

--- main.js
+++ main.js
@@ -29,14 +29,14 @@ const changeDate = function (event) {
   const selectedMonth = Number(document.querySelector('#monthSelect').value)
   const yearInput = Number(document.querySelector('#yearInput').value)
   dateList = []
-  render(new Date(yearInput, selectedMonth, 1))
+  render(new Date(yearInput, selectedMonth))
 }
 
 const render = function (dt = null) {
   calendarBody.innerHTML = ''
 
   let daysInMonth = getDaysInMonth(dt.getMonth(), dt.getFullYear())
-  for (let week = 0; week < Math.ceil(daysInMonth / 7 ); week++) {
+  for (let week = 0; week < Math.ceil(daysInMonth / 6 ); week++) {
     dateList.push([])
   }
 
@@ -45,25 +45,34 @@ const render = function (dt = null) {
     let newDate = new Date(dt.getFullYear(), dt.getMonth(), i + 1)
     if (i === 0) {
       for (let emptyDate = newDate.getDay() - 2; emptyDate >= 0; emptyDate--) {
-        let prevMonth = getDaysInMonth(dt.getMonth() + 1, dt.getFullYear())
+        let nextM = dt.getMonth() === 11 ? 0 : dt.getMonth() + 1
+        let prevMonth = getDaysInMonth(nextM, dt.getFullYear())
         dateList[ week ].push(new Date(dt.getFullYear(), dt.getMonth() - 1, prevMonth - emptyDate))
       }
       dateList[ week ].push( newDate )
-    } else {
+    } else if ( dateList[ week ] !== undefined ){
       dateList[ week ].push( newDate )
     }
 
-    if (dateList[ week ].length === 7) {
+    if ( dateList[ week ].length === 7 ) {
       week++
     }
   }
 
+  dateList.map(( d, i) => {
+    if (d.length === 0) {
+      dateList.splice( i, 1)
+    }
+  })
+
   let nextMonthDates = []
   if ( dateList[ dateList.length - 1 ].length !== 7 ) {
     for (let i = 0; i < (7 - dateList[ dateList.length - 1 ].length) ; i++) {
-      nextMonthDates.push(new Date(dt.getFullYear(), dt.getMonth() + 1, i + 1))
+      let nextM = dt.getMonth() === 11 ? 0 : dt.getMonth() + 1
+      nextMonthDates.push(new Date(dt.getFullYear(), nextM, i + 1))
     }
   }
+
   dateList[ dateList.length - 1 ] = dateList[ dateList.length - 1 ].concat(nextMonthDates)
 
   dateList.map(( week, weekIndex) => {
@@ -76,6 +85,9 @@ const render = function (dt = null) {
       let newDate = document.createElement('div')
       newDate.id = `date${ dateIndex + 1 }`
       newDate.className = 'box'
+      if (dateIndex === 6) {
+        newDate.className = `${ newDate.className } sunday`
+      }
       if (dt.getMonth() !== currentDate.getMonth()) {
         newDate.className = `${ newDate.className } off`
       }
